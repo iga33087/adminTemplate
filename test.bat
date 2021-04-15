44dabda71af7d5c99580d714287bdecd2dd4cc08
iga33087 <iga33087@gmail.com>
2021-04-16 02:30:15 +0800

    bd

--- test.bat
+++ test.bat
@@ -1,10 +1,13 @@
 echo "123456789"
 call npm run build
 cd dist
+git checkout gh-pages
+git add favicon.ico -f
 ::for /r %%i in (*) do (
 ::  echo %%i
 ::  git add %%i -f
 ::)
-git checkout gh-pages
+git commit 123 -m
+git push
 git status
 pause
